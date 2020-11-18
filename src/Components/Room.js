376c46564a8933de90a2b417d6984f00000eab5d
srajvanshi3 <shivam99rajvanshi@gmail.com>
2020-11-18 17:26:54 +0530

    second

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -3,12 +3,11 @@ import {Link } from 'react-router-dom'
 
 function Room({room}) {
     const {name , slug  ,images , price}= room;
-    console.log(images)
     return (
         <article className='room'>
 
         <div className='img-container'>
-            <img src={images[0]} alt='single-image'/>
+            <img src={images[0]} alt='single-room'/>
             <div className='price-top'>
     <h6>Rs{price}</h6>
     <p>per night</p>
